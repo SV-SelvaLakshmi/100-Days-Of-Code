32253fb2281c5618165533046cfa9e8b11fa87b5
Selva Lakshmi <53254307+SV-SelvaLakshmi@users.noreply.github.com>
2021-04-15 01:13:46 +0530

    Printing columns according to rows value
    Small change : j<=i

---
+++ 7_Printing columns according to rows.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{   for(int i=1;i<=5;i++,printf("\n")){
+       for(int j=1;j<=i;j++)
+       {
+           printf("%d ",j);
+       }
+    }
+    return 0;
+}
+/*Output
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+*/
