df32bb8aa1f4c0d1fd7bf60b5fa8525425bf8e5d
Selva Lakshmi <53254307+SV-SelvaLakshmi@users.noreply.github.com>
2021-04-15 00:32:38 +0530

    Star pattern.c

---
+++ 2_Starpattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{   for(int i=1;i<=5;i++,printf("\n"))
+    {
+     for(int j=1;j<=5;j++){
+       printf("* ");
+     }
+    }
+    return 0;
+}
+/*
+Output
+=========
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+=========
+*/
